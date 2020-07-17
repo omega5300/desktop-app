420dc08e22335a7f8f9bec7ebed481d8cd040ea1
Jheyson Saavedra <66929614+jheysonsz@users.noreply.github.com>
2020-07-17 15:27:55 -0500

    creando archivos de configuracion y index.ts

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+/* -------------------------------------------------------------------------
+ *  IMPORTACION DE MODULOS DE NPM   
+------------------------------------------------------------------------- */
+import { app, BrowserWindow } from "electron";
+import electronReload from "electron-reload";
+
+/* -- Modulos internos de node.js -- */
+
+/* -------------------------------------------------------------------------
+ *	COMIENZO DE LA APP
+------------------------------------------------------------------------- */
+let userLoginStatus: boolean = false;
+
+let homeWindow = null;
+let loginWindow = null;
+let registerWindow = null;
+
+app.on("ready", () => {
+  homeWindow = new BrowserWindow({
+    width: 800,
+    heigth: 600,
+  });
+});
